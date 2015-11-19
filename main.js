9af5d2b7ef5bccaa3f8d250e31288cf98b75eded
Josh <jfadem82@gmail.com>
2015-11-18 17:12:38 -0800

    builds javascript logic

---
+++ main.js
@@ -0,0 +1,8 @@
+var wdi_20_names = ["Alex", "Brandon", "Courtney", "Jessica", "Josh", "Kristin", "Miwa", "Shon", "TJ"];
+
+var i = 0;
+function eachName(){
+var x = Math.floor((Math.random()) * wdi_20_names.length)
+console.log(wdi_20_names[x])
+}
+eachName();
