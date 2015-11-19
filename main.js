6ed17df01c4df37070943922dfb40a7cc72b5019
Josh <jfadem82@gmail.com>
2015-11-18 18:36:36 -0800

    builds bonus challenge

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 var wdi_20_names = ["Alex", "Brandon", "Courtney", "Jessica", "Josh", "Kristin", "Miwa", "Shon", "TJ"];
-
-var i = 0;
-function eachName(){
-var x = Math.floor((Math.random()) * wdi_20_names.length)
-console.log(wdi_20_names[x])
+function eachName() {
+var x = Math.floor((Math.random() * wdi_20_names.length));
+console.log(wdi_20_names[x]);
+wdi_20_names.splice(x, 1);
+if (wdi_20_names.length === 0){
+	wdi_20_names.push("Alex", "Brandon", "Courtney", "Jessica", "Josh", "Kristin", "Miwa", "Shon", "TJ");
+	}
 }
-eachName();
+
